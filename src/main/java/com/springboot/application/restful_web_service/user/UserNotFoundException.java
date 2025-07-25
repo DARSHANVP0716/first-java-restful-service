96d82865e9d246ce64799fef358e038503900ac3
Darshan Patil <darshanvp2@gmail.com>
2025-07-25 08:28:31 +0530

    UserNotFoundException.java has been updated

--- src/main/java/com/springboot/application/restful_web_service/user/UserNotFoundException.java
+++ src/main/java/com/springboot/application/restful_web_service/user/UserNotFoundException.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code= HttpStatus.NOT_FOUND)  //whenever this exception occurs (throw in controller) spring throws
                                               // an error code status with error using @ResponseStatus annotation
 public class UserNotFoundException extends RuntimeException {   //all these classes will be handled by spring boot
-    public UserNotFoundException(String message){
-        //this below super method is there in super class so we are just sending it
-        super(message);  //message is passes to the parent class, so it shows this message in response body
-    }
+//    public UserNotFoundException(String message){
+//        //this below super method is there in super class so we are just sending it
+//        super(message);  //message is passes to the parent class, so it shows this message in response body
+//    }
 }
 
